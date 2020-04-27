d1a0ee90f6277615d165fc5dfa92bf4b22a07fb1
Edwin Lee <leeed2001@gmail.com>
2020-04-27 17:09:40 -0500

    Initial commit

---
+++ f2p/test/test_dummy.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+class TestDummy(TestCase):
+
+    def test_a(self):
+        self.assertEqual(0, 0)
+
+    def test_b(self):
+        self.assertEqual(0, 0)
+
+    def test_c(self):
+        self.assertEqual(0, 0)
+
+    def test_d(self):
+        self.assertEqual(0, 0)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EnergyPlusFortranToPython',
+    version='0.1',
+    packages=['f2p'],
+    url='',
+    license='',
+    author='Edwin Lee',
+    author_email='',
+    description=''
+)
